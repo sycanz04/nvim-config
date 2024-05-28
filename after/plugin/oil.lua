3c3f7118523e62677728613cf23ae62b15de633a
sycanz04 <aidenchan0397@gmail.com>
2024-05-28 08:20:01 +0800

    fix: Can show hidden files now

--- after/plugin/oil.lua
+++ after/plugin/oil.lua
@@ -4,4 +4,17 @@ require("oil").setup({
         "icon",
         "permissions",
     },
+    skip_confirm_for_simple_edits = false,
+    prompt_save_on_select_new_entry = true,
+    view_options = {
+        show_hidden = true,
+
+        is_hidden_file = function(name, bufnr)
+            return vim.startswith(name, ".")
+        end,
+
+        is_always_hidden = function(name, bufnr)
+            return false
+        end,
+    }
 })
