bc7e6da1c02ce0bf9373e300856333c7489a388b
sycanz04 <aidenchan0397@gmail.com>
2024-05-28 09:44:20 +0800

    fix: small changes to confirm edit

--- after/plugin/oil.lua
+++ after/plugin/oil.lua
@@ -4,7 +4,7 @@ require("oil").setup({
         "icon",
         "permissions",
     },
-    skip_confirm_for_simple_edits = false,
+    skip_confirm_for_simple_edits = true,
     prompt_save_on_select_new_entry = true,
     view_options = {
         show_hidden = true,
